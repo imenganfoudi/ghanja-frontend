be7ec9b4b25b63e8b5528dbdc1140ecd29037bcf
imen <imenganfoudi123@gmail.com>
2026-09-02 11:26:31 +0200

    Point axios to production API URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+baseURL: 'https://ghanja-v2-api.onrender.com/api',
   withCredentials: false,
   headers: {
     'Accept': 'application/json',
